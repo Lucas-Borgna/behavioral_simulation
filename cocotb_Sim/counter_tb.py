20d1249c36c27e8dadcf9ccf8abd373a905f4f12
Lucas-Borgna <lucas.borgna.16@ucl.ac.uk>
2019-08-12 13:40:33 +0200

    updated makefile and testbench

--- cocotb_Sim/counter_tb.py
+++ cocotb_Sim/counter_tb.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+from __future__ import division
+
 import cocotb
 from cocotb.triggers import Timer
 from cocotb.clock import Clock
@@ -11,4 +14,16 @@ def reset_dut(reset_n, duration):
 
 @cocotb.test()
 def my_first_test(dut):
-	cocotb.fork(Clock(dut.clock, 20, units='ns').start())
+	dut.clock <= 0
+	dut.reset <= 0
+	dut.up_down <= 0
+	#cocotb.fork(Clock(dut.clock, 20, units='ns').start()
+
+	dut._log.info('about to wait for 20ns')
+	yield Timer(20, units = "ns")
+	dut._log.info('Simulation time has advanced 20ns')
+	check = dut.reset.value
+	dut._log.info(check.binstr)
+	
+
+
